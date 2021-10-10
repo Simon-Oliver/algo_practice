bf096adf9d52bb4f8356ff7350822aa483942466
Simon-Oliver <stauffer.simon@outlook.com>
2021-10-10 15:26:40 +1100

    managed to list devices

--- js/ble.mjs
+++ js/ble.mjs
@@ -1,5 +1,14 @@
 // Read the battery level of the first found peripheral exposing the Battery Level characteristic
-
 import noble from "@abandonware/noble";
 
-noble.on("discover", console.log(peripheral));
+noble.on("stateChange", function (state) {
+  if (state === "poweredOn") {
+    noble.startScanning();
+  } else {
+    noble.stopScanning();
+  }
+});
+
+noble.on("discover", function (peripheral) {
+  console.log(peripheral);
+});
