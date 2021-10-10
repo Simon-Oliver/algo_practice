3873839c0f0ecb7bfeac83ef6934cb5f54535f67
Simon-Oliver <stauffer.simon@outlook.com>
2021-10-10 21:50:39 +1100

    exploring oop

--- js/oop.mjs
+++ js/oop.mjs
@@ -1,9 +1,18 @@
 import fs from "fs";
 
-class Loggger {
-  constructor(func) {}
-  Start() {
-    fs.writeFile();
-    func();
+class Logger {
+  constructor(func) {
+    this.func = func;
+  }
+  Log(path, data) {
+    fs.writeFile(path, data, () => console.log("File Saved"));
+    this.func();
   }
 }
+
+const log1 = new Logger(() => console.log("Saved"));
+
+log1.Log(
+  "./data.txt",
+  JSON.stringify({ test: "Hello World", data1: "Hello World 2" })
+);
