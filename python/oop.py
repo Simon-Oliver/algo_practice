80eb486114d7aac8b1038d8431c61823b8c2a882
Simon-Oliver <stauffer.simon@outlook.com>
2021-11-28 00:24:38 +1100

    oop

--- python/oop.py
+++ python/oop.py
@@ -2,3 +2,10 @@ class Logger():
     def __init__(self, path,data):
         self.path = path
         self.data = data
+
+class Printer():
+    def __init__(self, data):
+        print("--------",data)
+
+
+p = Printer({"name":"test"})
