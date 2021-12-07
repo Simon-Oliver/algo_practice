0386780683d6bdfb4f35c79de5dd2eb3e3990a26
Simon-Oliver <stauffer.simon@outlook.com>
2021-12-07 18:53:37 +1100

    budget class

--- python/budget.py
+++ python/budget.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Budget():
     def __init__(self, category, init_budget=None):
         self.ledger = []
@@ -24,16 +27,31 @@ class Budget():
             return True
         return False
 
-
-
     def transfer(self,amount,dest_budget):
         if self.withdraw(amount, 'Transfer to {}'.format(dest_budget.category)):
             dest_budget.deposit(amount,'Transfer from {}'.format(self.category))
             return True
         return False
 
+    def center_padding(self,total_len,char,str):
+        front = char * (math.floor((total_len-len(str))/2))
+        back = char * (math.ceil((total_len-len(str))/2))
+        return f'{front}{str}{back}'
 
-food = Budget('Food',5000)
+    def padding_between(self,left,right=None):
+        if right is not None:
+            return f"{left.ljust(23)[:23]}{str(format(right,'.2f')).rjust(7)[:7]}"
+        return f"{left.ljust(30)[:30]}"
+
+    def __str__(self):
+        new_str = self.center_padding(30,"*",self.category) + "\n"
+        for e in self.ledger:
+            new_str += self.padding_between(e["description"],e["amount"]) + "\n"
+        new_str += self.padding_between(f'Total: {self.get_balance():.2f}')
+        return new_str
+        
+
+food = Budget('Food',5000.548)
 food.deposit(800,'Pay')
 bills = Budget('Bills')
 
@@ -42,3 +60,4 @@ bills = Budget('Bills')
 food.transfer(1000,bills)
 print("Food",food.ledger)
 print("Bills",bills.ledger)
+print(food)
