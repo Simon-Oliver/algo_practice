62aa4ca7f0c47ece79d3ff7a71e6721bf54296cc
Simon-Oliver <stauffer.simon@outlook.com>
2021-12-28 20:11:31 +1100

    sql

--- python/sql.py
+++ python/sql.py
@@ -4,10 +4,33 @@ con = sqlite3.connect("../chinook.db")
 cur = con.cursor()
 
 cur.execute('''
+INSERT INTO employees(
+   LastName,
+   FirstName,
+   Country
+) VALUES(
+    "Muster",
+    "Max",
+    "Australia"
+    ) 
+''')
+
+cur.execute('''
 SELECT 
-    EmployeeId, COUNT(ReportsTo)
+    country, COUNT(Country)
 FROM 
     employees
+GROUP By country
 ''')
+
+# cur.execute('''
+# SELECT 
+#     *
+# FROM 
+#     employees
+# ''')
+
+con.commit()
+
 result = cur.fetchall()
 print(result)
