a784f134c547e98fe51b0cd9f0be690d01aab99e
Simon-Oliver <stauffer.simon@outlook.com>
2022-01-20 21:11:00 +1100

    Exploring simple terminal programs with submenus

--- python/input.py
+++ python/input.py
@@ -1,57 +1,73 @@
 import sqlite3
 from os import system
 
-# con = sqlite3.connect("../chinook.db")
-# cur = con.cursor()
+con = sqlite3.connect("../chinook.db")
+cur = con.cursor()
 
-# menu = {1: "Search by CustomerId", 2: "Search by LastName", 3: "Search by Email"}
-
-
-# def create_menu():
-#     for key in menu.keys():
-#         print(f"{key} - {menu[key]}")
+menu = {1: "Search by CustomerId", 2: "Search by LastName", 3: "Search by Email"}
 
 
-# while True:
-#     create_menu()
-#     option = int(input("Select search: "))
-#     match option:
-#         case 1:
-#             search = input("Enter CustomerId: ")
-#             cur.execute(f"SELECT LastName from customers WHERE CustomerId = {search}")
-#             res = cur.fetchall()
-#             print(res[0][0])
+def search_CustomerId():
+    search = input("Enter CustomerId: ")
+    cur.execute(f"SELECT LastName from customers WHERE CustomerId = {search}")
+    res = cur.fetchall()
+    print(res[0][0])
 
+def create_menu():
+    for key in menu.keys():
+        print(f"{key} - {menu[key]}")
+    option = int(input("Select search: "))
+    match option:
+        case 1:
+            search_CustomerId()
+            create_back(search_CustomerId)
 
-
-
-
-def settings():
-    system('clear')
+def create_back(searchFunction):
     print(f'''
-    1 - Some Setting
-    2 - Back
-    ''',end="\r")
+    1 - Back to Menu
+    2 - Search again
+    ''')
     select = int(input('Select Option: '))
     match select:
         case 1:
-            print("Some setting has been toggled")
+            create_menu()
         case 2:
-             menu()
+            searchFunction()
+            create_back(searchFunction)
 
-def menu():
-    system('clear')
-    print(
-        f"""
-    1 - Settings
-    2 - Exit
-    """
-    ,end="\r")
-    select = int(input('Select Option: '))
-    match select:
-        case 1:
-             settings()
-        case 2:
-            exit()
+def run():
+    create_menu()
+    
 
-menu()
+
+run()
+
+# def settings():
+#     system('clear')
+#     print(f'''
+#     1 - Some Setting
+#     2 - Back
+#     ''',end="\r")
+#     select = int(input('Select Option: '))
+#     match select:
+#         case 1:
+#             print("Some setting has been toggled")
+#         case 2:
+#              menu()
+
+# def menu():
+#     system('clear')
+#     print(
+#         f"""
+#     1 - Settings
+#     2 - Exit
+#     """
+#     ,end="\r")
+#     select = int(input('Select Option: '))
+#     match select:
+#         case 1:
+#              settings()
+#         case 2:
+#             exit()
+
+# menu()
