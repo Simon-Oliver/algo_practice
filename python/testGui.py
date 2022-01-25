0d44a0435f149df2fb41bd2e45d2228d4dbd85c7
Simon-Oliver <stauffer.simon@outlook.com>
2022-01-25 18:12:42 +1100

    very simple gui connnected to sqlite

--- python/testGui.py
+++ python/testGui.py
@@ -14,20 +14,35 @@
 
 import tkinter
 import customtkinter  # <- import the CustomTkinter module
+import sqlite3
+
+con = sqlite3.connect("../chinook.db")
+cur = con.cursor()
 
 root_tk = tkinter.Tk()  # create the Tk window like you normally do
-root_tk.geometry("400x400")
+root_tk.geometry("800x680")
 root_tk.title("CustomTkinter Test")
 
-
-
 def button_function():
-    print("button pressed")
+    cur.execute(f"SELECT LastName from customers WHERE CustomerId = {entry_1.get()}")
+    res = cur.fetchall()
+    label_1.set_text(res[0][0])
+    entry_1.delete(0,"end")
 
-WIDTH = 700
-HEIGHT = 500
 
-frame_top = customtkinter.CTkFrame(master=root_tk, corner_radius=10)
-frame_top.place(height=200, width=200, relx=0.5, anchor=tkinter.CENTER)
+
+y_padding = 13
+
+frame_1 = customtkinter.CTkFrame(master=root_tk, corner_radius=15)
+frame_1.pack(pady=20, padx=60, fill="both", expand=True)
+
+entry_1 = customtkinter.CTkEntry(master=frame_1)
+entry_1.pack(pady=y_padding, padx=10)
+
+button_1 = customtkinter.CTkButton(master=frame_1, corner_radius=10, command=button_function)
+button_1.pack(pady=y_padding, padx=10)
+
+label_1 = customtkinter.CTkLabel(master=frame_1, text="")
+label_1.pack(pady=y_padding, padx=10)
 
 root_tk.mainloop()
