6cc308fecdea0e3bbeecca19907dc59df635d29b
Simon-Oliver <stauffer.simon@outlook.com>
2022-02-03 18:03:22 +1100

    exploring class attributes

--- python/oop.py
+++ python/oop.py
@@ -1,4 +1,7 @@
 class Item:
+    # class attributes
+    pay_rate = 0.5  # Setting an initial sales price at 50% off
+
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations
         assert price >= 0, f"Price {price} can't be less than 0."
@@ -9,12 +12,25 @@ class Item:
         self.quantity = quantity
         self.total_price = self.calculate_total_price()
 
-        def set_price
-
     def calculate_total_price(self):
         return self.quantity * self.price
 
-item1 = Item("Iphone", 2834,33)
+    def apply_discount(self):
+        # Revering to self.pay_rate will make sure the instance pay rate
+        # is used when applied specifically to an instance
+        self.price = self.price * self.pay_rate
+
+    def remove_discount(self):
+        self.price = self.price / self.pay_rate
+
+
+item1 = Item("Iphone", 100, 5)
 print(item1.calculate_total_price())
 
-
+print(item1.price)
+# Adding a different sales rate to specific instance
+item1.pay_rate = 0.8
+item1.apply_discount()
+print("Sales applied", item1.price)
+item1.remove_discount()
+print("Discount removed", item1.price)
